0cc411bca090b063c6dab3cc40bf1313809b397b
Jenny Allar <JennyAllar@gmail.com>
2014-05-24 22:10:01 -0600

    User can log in.

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -14,4 +14,24 @@ feature 'User functions' do
     
     expect(page).to have_content 'Welcome, Bob!'
   end
+  
+  scenario 'allows a user to sign in' do
+    visit '/'
+    click_on 'Register Here!'
+    fill_in 'First Name', with: 'Bob'
+    fill_in 'Last Name', with: 'Robert'
+    fill_in 'Email', with: 'BobRobert@bob.com'
+    fill_in 'Password', with: 'bob'
+    fill_in 'Password Confirmation', with: 'bob'
+    check 'terms'
+    click_on 'Submit'
+
+    expect(page).to have_content 'Welcome, Bob!'
+    click_on 'Sign Out'
+    
+    click_on 'Sign In'
+    fill_in 'Email', with: 'BobRober@bob.com'
+    fill_in 'Password', with: 'bob'
+    click_on 'Submit'
+  end
 end
